bacbb72b9a6582d1a1602ffd7c14dc55bbd4b858
samuelk14 <32883598+samuelk14@users.noreply.github.com>
2018-03-02 02:07:54 -0500

    newchanges

--- app/src/main/java/proyecto2/app1/com/example/pc/c175/MainActivity.java
+++ app/src/main/java/proyecto2/app1/com/example/pc/c175/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 
@@ -29,6 +30,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_nav, menu);
+        return true;
+    }
 
     @Override
     public void onClick(View view) {
--- app/src/main/java/proyecto2/app1/com/example/pc/c175/MainNavActivity.java
+++ app/src/main/java/proyecto2/app1/com/example/pc/c175/MainNavActivity.java
@@ -59,12 +59,7 @@ public class MainNavActivity extends AppCompatActivity
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main_nav, menu);
-        return true;
-    }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
